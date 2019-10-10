c03b5dc914841e80734fe7474b07323561e6c70d
ericpkatz <ericpkatz@gmail.com>
2019-10-10 19:30:41 -0400

    added Thing model

--- spec.js
+++ spec.js
@@ -1,7 +1,9 @@
 const app = require('supertest')(require('./app'));
 const { expect } = require('chai');
+const db = require('./db');
 
 describe('Our Application', ()=> {
+  beforeEach(()=> db.syncAndSeed());
   it('true is true', ()=> {
     expect(true).to.equal(true);
   });
@@ -9,7 +11,7 @@ describe('Our Application', ()=> {
     it('returns a 200 with foo', async()=> {
       const response = await app.get('/');
       expect(response.status).to.equal(200);
-      expect(response.text).to.contain('foo');
+      expect(response.body.name).to.contain('foo');
     });
   });
 });
